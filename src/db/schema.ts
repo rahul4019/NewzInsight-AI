52e723e4653cf6f7b197de0e2aa75dc5707daf90
rahul4019 <rahulkashyap4019@gmail.com>
2024-12-27 14:35:58 +0530

    Update uuid in analysis table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,9 @@
 import { json, pgTable, text, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const articleAnalysesTable = pgTable("article_analyses", {
-  id: varchar("id").primaryKey().default(crypto.randomUUID()),
+  id: varchar("id")
+    .primaryKey()
+    .$default(() => crypto.randomUUID()),
   title: text("title").notNull(),
   articleLink: text("article_link").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
