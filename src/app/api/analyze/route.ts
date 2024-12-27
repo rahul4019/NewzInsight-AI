3991f801c81be454c31f9afd460f15ec41f3de6f
rahul4019 <rahulkashyap4019@gmail.com>
2024-12-27 14:21:40 +0530

    Implement logic for only new urls will be analyised by the llm

--- src/app/api/analyze/route.ts
+++ src/app/api/analyze/route.ts
@@ -3,6 +3,7 @@ import fetchAndExtractArticle from "@/utils/fetchAndExtractArticle";
 import getNewsInsight from "@/utils/getNewsInsight";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { articleAnalysesTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export async function POST(request: NextRequest) {
   try {
@@ -10,6 +11,16 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
 
     const articleUrl = body;
+
+    // only anlayse the article if it is not present in the db
+    const articleAnalysisExist = await db
+      .select()
+      .from(articleAnalysesTable)
+      .where(eq(articleAnalysesTable.articleLink, articleUrl));
+
+    if (articleAnalysisExist.length > 0) {
+      return NextResponse.json({ ...articleAnalysisExist[0] }, { status: 200 });
+    }
 
     const articleContent = await fetchAndExtractArticle(articleUrl);
 
