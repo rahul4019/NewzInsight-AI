6f5ffbb45a540897f01b4a8024ea8d2aa3d352dd
rahul4019 <rahulkashyap4019@gmail.com>
2024-12-27 22:39:10 +0530

    Add suspense for recent analysis section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import AnalysisSection from "@/components/sections/AnalysisSection";
 import { FeaturesSection } from "@/components/sections/FeaturesSection";
 import { HeroSection } from "@/components/sections/HeroSection";
 import { RecentAnalysisSection } from "@/components/sections/RecentAnalysisSection";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <AnalysisSection />
-      <RecentAnalysisSection />
+      <Suspense fallback={<div>Loading...</div>}>
+        <RecentAnalysisSection />
+      </Suspense>
       <FeaturesSection />
     </div>
   );
